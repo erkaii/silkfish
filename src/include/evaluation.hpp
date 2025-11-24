7bb778d4bf2ac90acaf05813f339790913fc0772
Erkai Yu <si1krow@outlook.com>
2025-11-23 19:27:37 -0600

    rebuilding

--- src/include/evaluation.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "chess.hpp"
-
-int evaluation(chess::Board& board);
-bool appear_quiet(chess::Board board);
--- src/include/search.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "chess.hpp"
-#include "thread_pool.hpp"
-#include <algorithm>
-std::pair<int, std::string> quiescence_search(int q_depth, int alpha, int beta, chess::Color color, chess::Board board);
-std::pair<int, std::string> minimax(int mm_depth, int alpha, int beta, chess::Color color, chess::Board board);
-chess::Move findBestMove(chess::Board& board, int depth, int max_threads);
